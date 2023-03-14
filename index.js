03bdd3af3300ae19a52ba5ae6ca09222383dd74e
Njalale <fnjalale@gmail.com>
2023-03-14 20:23:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+let street = 43
+function distanceFromHqInBlocks(street){
+    if (street === 43){
+        return 1
+    }
+    street = 50
+    if (street === 50){
+        return 8
+    }
+    street < 42
+    if (street < 42){
+        return 8
+    }
+}
+street = 43
+function distanceFromHqInFeet(street){
+    if (street === 43){
+    return 264
+    }
+    street = 50
+    if (street === 50){
+        return 2112
+    }
+    street <42 
+    if (street < 42){
+        return 2112
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    switch(start, destination){
+        case (43, 48):
+            return 1320
+        case (50,60):
+            return 2640
+        case (34, 28):
+            return 1584
+    }
+}  
+ 
+ function calculatesFarePrice (start, destination){
+    switch(start, destination){
+        case (43, 44):
+            return 0
+        case (34,32):
+            return 2.56
+        case (50, 58):
+            return 25
+        case (34,24):
+            return 'cannot travel that far'
+    }
+ }
+    
+    
